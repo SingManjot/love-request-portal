eeb2896a2d06a5aa9ef02be6dfc61e0fd856af28
SingManjot <73088810+SingManjot@users.noreply.github.com>
2025-04-14 15:30:10 +0530

    Update NewRequest.tsx changed value

--- src/pages/NewRequest.tsx
+++ src/pages/NewRequest.tsx
@@ -25,7 +25,7 @@ const NewRequest = () => {
     wished_date: "",
     wished_urgency: "medium" as "low" | "medium" | "high",
     reason: "",
-    person: "Smrthi" as "Smrthi" | "Gurleen" | "Nandini"
+    person: "her" as "Smrthi" | "Gurleen" | "Nandini"
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
